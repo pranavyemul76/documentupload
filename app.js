74500f5580934df2ac7ffe6a5393f6545d5307cc
deepaklegitbytes <127582687+deepaklegitbytes@users.noreply.github.com>
2024-09-29 12:52:28 +0530

    vercel config setup path require

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const port = process.env.PORT || 8080;
 const host = process.env.VERCEL_URL;
+const path = require("path");
 const cors = require("cors");
 const Route = require("./Router");
 const mongoose = require("mongoose");
